ef4755bf3a990a91b778b50f5953e9ae4b42be61
brunovskyoliver <obrunovsky7@gmail.com>
2025-09-04 08:38:21 +0200

    Prepare for Vercel deployment

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { ToastProvider } from "@/components/toast-provider";
 import { Analytics } from '@vercel/analytics/next';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import "./globals.css";
 
 const geistSans = Geist({
@@ -33,6 +34,7 @@ export default function RootLayout({
         <ToastProvider />
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
